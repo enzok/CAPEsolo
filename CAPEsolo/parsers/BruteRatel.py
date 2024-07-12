ba2a5df67a65fecb8a98bb02d8c670dcbaaaa692
enzok <7831008+enzok@users.noreply.github.com>
2024-07-12 13:37:02 -0400

    Updates

--- CAPEsolo/parsers/BruteRatel.py
+++ CAPEsolo/parsers/BruteRatel.py
@@ -1,21 +1,21 @@
+from contextlib import suppress
+
+
 def extract_config(data):
     config_dict = {}
 
-    try:
+    with suppress(Exception):
+        i = 0
         lines = data.decode().split("\n")
-    except Exception:
-        return
-
-    i = 0
-    for line in lines:
-        if line.startswith("Mozilla"):
-            config_dict["User Agent"] = line
-            config_dict["C2"] = list(set(lines[i-2].split(",")))
-            config_dict["C2 Port"] = lines[i-1]
-            config_dict["URI"] = lines[i+3].split(",")
-            config_dict["Keys"] = [lines[i+1], lines[i+2]]
-            break
-        i += 1
+        for line in lines:
+            if line.startswith("Mozilla"):
+                config_dict["User Agent"] = line
+                config_dict["C2"] = list(set(lines[i - 2].split(",")))
+                config_dict["Port"] = lines[i - 1]
+                config_dict["URI"] = lines[i + 3].split(",")
+                config_dict["Keys"] = [lines[i + 1], lines[i + 2]]
+                break
+            i += 1
 
     return config_dict
 
--- CAPEsolo/yara/CAPE/BruteRatel.yar
+++ CAPEsolo/yara/CAPE/BruteRatel.yar
@@ -2,11 +2,13 @@ rule BruteRatel
 {
     meta:
         author = "kevoreilly"
-        description = "BruteRatel Config"
+        description = "BruteRatel Payload"
         cape_type = "BruteRatel Payload"
     strings:
-        $config = {41 57 41 56 41 55 41 54 55 57 56 53 48 81 EC 28 01 00 00 48 83 B9 28 03 01 00 00}
-        $decode = {55 57 56 53 48 83 EC ?? 31 C0 48 89 CB 48 89 D7 44 89 C6 44 89 CD 44 39 01 77 ?? 41 8D 48 01 E8 [4] 31 C9}
+        $syscall1 = {49 89 CA 4? 89 ?? (41 FF|FF)}
+        $syscall2 = {49 89 CA 48 8B 44 24 ?? FF 64 24}
+        $jmpapi = {49 89 ?? 10 49 C7 45 08 ?? 00 00 00 E8 00 00 00 00 ?? (48|49) 83 [2] 41 FF E2}
+        $decode = {89 C2 8A 14 17 40 38 EA 75 06 FF C0 89 03 EB 0B 41 88 14 08 48 FF C1 FF 03 EB}
     condition:
-        all of them
+        2 of them
 }
