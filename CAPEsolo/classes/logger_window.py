fe46a14b6639884f0a9951e0af7fba88afe186a6
enzok <7831008+enzok@users.noreply.github.com>
2024-07-24 08:25:42 -0400

    log directly to analysis.log concurrently

--- CAPEsolo/classes/logger_window.py
+++ CAPEsolo/classes/logger_window.py
@@ -20,6 +20,8 @@ class LoggerWindow(wx.Frame, KeyEventHandlerMixin):
         self, parent, title, main_window_position, main_window_size, *args, **kwargs
     ):
         super(LoggerWindow, self).__init__(parent, title=title, *args, **kwargs)
+        self.analysisDir = parent.analysisDir
+        self.analysisLogPath = parent.analysisLogPath
         self.BindKeyEvents()
         self.main_window_position = main_window_position
         self.main_window_size = main_window_size
@@ -37,35 +39,36 @@ class LoggerWindow(wx.Frame, KeyEventHandlerMixin):
         )
         self.resultsWindow.SetFont(fontCourier)
         vbox.Add(self.resultsWindow, proportion=1, flag=wx.EXPAND | wx.ALL, border=5)
-        saveBtn = wx.Button(panel, label="Save Log")
-        saveBtn.Bind(wx.EVT_BUTTON, self.OnSaveLog)
-        vbox.Add(saveBtn, proportion=0, flag=wx.ALL | wx.CENTER, border=5)
+        copyPathBtn = wx.Button(panel, label="Copy Log")
+        copyPathBtn.Bind(wx.EVT_BUTTON, self.OnCopyPath)
+        vbox.Add(copyPathBtn, proportion=0, flag=wx.ALL | wx.CENTER, border=5)
         panel.SetSizer(vbox)
 
-        handler = WxTextCtrlHandler(self.resultsWindow)
+        fileHandler = logging.FileHandler(self.analysisLogPath)
+        wxHandler = WxTextCtrlHandler(self.resultsWindow)
         logging.basicConfig(
             level=logging.DEBUG,
-            handlers=[handler],
+            handlers=[fileHandler, wxHandler],
             format="%(asctime)s [%(name)s] %(levelname)s: %(message)s",
         )
 
         self.main_window_position.x += self.main_window_size.x
         screenWidth, _ = wx.DisplaySize()
-        self.SetSize(int(screenWidth * 0.98 - self.main_window_size.x), self.main_window_size.y)
+        self.SetSize(
+            int(screenWidth * 0.98 - self.main_window_size.x), self.main_window_size.y
+        )
         self.SetPosition(self.main_window_position)
 
-    def OnSaveLog(self, event):
-        with wx.FileDialog(
-            self,
-            "Save log as...",
-            wildcard="Text files (*.txt)|*.txt",
-            style=wx.FD_SAVE | wx.FD_OVERWRITE_PROMPT,
-        ) as fileDialog:
-            if fileDialog.ShowModal() == wx.ID_CANCEL:
-                return
-            pathname = fileDialog.GetPath()
-            try:
-                with open(pathname, "w") as file:
-                    file.write(self.resultsWindow.GetValue())
-            except IOError:
-                wx.LogError(f"Cannot save log to file '{pathname}'.")
+    def OnCopyPath(self, event):
+        if wx.TheClipboard.Open():
+            file_data = wx.FileDataObject()
+            file_data.AddFile(self.analysisLogPath)
+            wx.TheClipboard.SetData(file_data)
+            wx.TheClipboard.Close()
+            wx.MessageBox(
+                f"Analysis log copied: {self.analysisLogPath}",
+                "Info",
+                wx.OK | wx.ICON_INFORMATION,
+            )
+        else:
+            wx.LogError("Unable to open the clipboard.")
